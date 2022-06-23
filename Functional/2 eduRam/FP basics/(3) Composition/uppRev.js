1a727d74c64ac0c81cf5b6b1562ed88efd2897ba
Erick Lozano <erickklozano3@gmail.com>
2022-06-23 18:31:41 -0500

    added more exc folders + 3.1 comp exc

---
+++ Functional/2 eduRam/FP basics/(3) Composition/uppRev.js
@@ -0,0 +1,23 @@
+//Make a function that uppercases and reverses ONE name
+const bobo = {
+  firstName: 'Bobo',
+  lastName: 'Flakes'
+};
+
+const result = upperAndReverseFirstName(bobo);
+console.log({ result })
+
+//Make a function that uppercases and reverses MULTIPLE names
+const users = [{
+  firstName: 'Bobo',
+  lastName: 'Flakes'
+}, {
+  firstName: 'Lawrence',
+  lastName: 'Shilling'
+}, {
+  firstName: 'Anon',
+  lastName: 'User'
+}];
+
+
+// Expected Output: ['OBOB', 'ECNERWAL', 'NONA']
