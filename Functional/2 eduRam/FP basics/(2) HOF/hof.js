3a67a6c97d7ba11a97ec23a0565c6afd7ff2afaf
Erick Lozano <erickklozano3@gmail.com>
2022-06-23 18:10:19 -0500

    finished edu ram 2.2 HOF exercise

--- Functional/2 eduRam/FP basics/(2) HOF/hof.js
+++ Functional/2 eduRam/FP basics/(2) HOF/hof.js
@@ -1,12 +1,9 @@
-/*const getMedianSixFigurePaycheck = pipe(
+/*const getMedianSixFigurePaycheck = R.pipe(
   getSalaries,
   reject(isBelowSixFigures),
   median,
   calculateMonthlyPaycheck,
   toUSD
 );
-
 const result = getMedianSixFigurePaycheck(employees);
-
 console.log({ result });*/
-
