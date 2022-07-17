826a830312c92d888621a2ecd41d25327b471ddc
Erick Lozano <erickklozano3@gmail.com>
2022-07-17 17:45:10 -0500

    added definitions to chal 2 for test purposes

--- no-bs-ts/challenges/chal-2.ts
+++ no-bs-ts/challenges/chal-2.ts
@@ -1,3 +1,8 @@
+/* Definitions:
+forEach - The forEach() method executes a provided function once for each array element.
+map - The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+filter - The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the
+given array that pass the test implemented by the provided function*/
+
 //Exercise: Re-implement forEach, map and filter using reduce, and to do it in a type safe manner using Typescript.
 //Link: https://youtu.be/DxG17pbgsZg
-
