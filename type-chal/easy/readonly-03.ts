ba9e2951ac0fac7ac52f5d537ee27b341bdd1aa2
Erick Lozano <erickklozano3@gmail.com>
2022-07-19 18:14:26 -0500

    finished type-chal/easy/readonly-03 + tuple-obj-4 exc

--- type-chal/easy/readonly-03.ts
+++ type-chal/easy/readonly-03.ts
@@ -3,16 +3,26 @@ Link: https://github.com/type-challenges/type-challenges/blob/main/questions/000
 Implement the built-in Readonly<T> generic without using it.
 Constructs a type with all properties of T set to readonly, meaning the properties of the constructed type cannot be reassigned.*/
 
-/*Example
-interface Todo {
+//Example
+interface Todo2 {
   title: string
   description: string
 }
-const todo: MyReadonly<Todo> = {
+const todo2: MyReadonly<Todo2> = {
   title: "Hey",
   description: "foobar"
 }
 
-todo.title = "Hello" // Error: cannot reassign a readonly property
-todo.description = "barFoo" // Error: cannot reassign a readonly property*/
+/*todo2.title = "Hello" // Error: cannot reassign a readonly property
+todo2.description = "barFoo" // Error: cannot reassign a readonly property*/
 
+
+//My Attempt - Correct
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P]
+}
+
+/*Solution
+type MyReadonly<T> = { 
+  readonly [K in keyof T]: T[K] 
+};*/
