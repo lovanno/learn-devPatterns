12f06ea12b09c672af02a6da0f0eb7c87a86aa06
Erick Lozano <erickklozano3@gmail.com>
2022-07-26 12:27:41 -0500

    finished type-chal/easy/push-12 + unshift-13 exc

--- type-chal/easy/push-12.ts
+++ type-chal/easy/push-12.ts
@@ -2,6 +2,14 @@
 Link: https://github.com/type-challenges/type-challenges/blob/main/questions/03057-easy-push/README.md
 Implement the generic version of Array.push*/
 
-/*Example:
-type Result = Push<[1, 2], '3'> // [1, 2, '3']*/
+//Example:
+type Result4 = Push<[1, 2], '3'> // [1, 2, '3']*/
 
+
+//My Attempt (Pass)
+type Push<T extends unknown[], U> = [...T, U];
+
+
+
+/*Solution: 
+type Push<T extends unknown[], U> = [...T, U];*/
--- type-chal/easy/unshift-13.ts
+++ type-chal/easy/unshift-13.ts
@@ -2,5 +2,13 @@
 Link: https://github.com/type-challenges/type-challenges/blob/main/questions/03060-easy-unshift/README.md
 Implement the type version of Array.unshift*/
 
-/*Example:
-type Result = Unshift<[1, 2], 0> // [0, 1, 2,]*/
+//Example:
+type Result6 = Unshift<[1, 2], 0> // [0, 1, 2,]*/
+
+
+//My Attempt (Pass)
+type Unshift<T extends unknown[], U> = [U, ...T];
+
+
+/*Solution
+type Unshift<T extends unknown[], U> = [U, ...T];*/
