3c944afac33ccdccffaf887c91d94eb81296e271
Erick Lozano <erickklozano3@gmail.com>
2022-07-26 12:40:01 -0500

    finished type-chal/easy/param-14 exc

--- type-chal/easy/param-14.ts
+++ type-chal/easy/param-14.ts
@@ -1,6 +1,21 @@
 /*                                    Parameters
 Implement the built-in Parameters generic without using it.*/
 
-/*Example:
+//Example:
 const foo = (arg1: string, arg2: number): void => { }
-type FunctionParamsType = MyParameters<typof foo > // [arg1: string, arg2: number]*/
+type FunctionParamsType = MyParameters<typeof foo> // [arg1: string, arg2: number]*/
+
+
+/*My Attempt (X)
+type MyParameters<T> = T extends Function(...args: any[]) => any ? never : never;
+
+Review:
+I was stuck on how to check the type without knowing it ahead of time. Using Infer was the solution*/
+
+
+
+//Solution 1
+type MyParameters<T> = T extends (...args: infer P) => any ? P : never;
+
+/*Solution 2
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer Args) => unknown ? Args : never*/
