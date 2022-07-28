3b9f75c3a5a3c74b540b03c6685c74b04001aa03
Erick Lozano <erickklozano3@gmail.com>
2022-07-27 23:22:06 -0500

    finished type-chal/04-medium/01-return-types + 02-omit exc

--- type-chal/02-medium/m-01-10/01-return-type.ts
+++ type-chal/02-medium/m-01-10/01-return-type.ts
@@ -3,12 +3,21 @@ Link: https://github.com/type-challenges/type-challenges/blob/main/questions/000
 Implement the built-in ReturnType<T> generic without using it
 A type takes the two arguments. The output should be a new array that includes inputs in ltr order*/
 
-/*Example:
+//Example:
 const fn = (v: boolean) => {
   if (v)
     return 1
   else
     return 2
 }
+type a = MyReturnType<typeof fn> // should be "1 | 2"
 
-type a = MyReturnType<typeof fn> // should be "1 | 2"*/
+
+//My Attempt (Pass)
+type MyReturnType<T> = T extends () => infer R ? R : T;
+
+/*Solution
+type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : T;*/
+
+
+/*Review: My solution would fail if a function with parameters is passed because it isn't assignable to type () => infer R*/
--- type-chal/02-medium/m-01-10/02-omit.ts
+++ type-chal/02-medium/m-01-10/02-omit.ts
@@ -3,15 +3,30 @@ Link: https://github.com/type-challenges/type-challenges/blob/main/questions/000
 Implement the built-in Omit<T, K> generic without using it.
 Constructs a type by picking all properties from T and then removing K*/
 
-/*Example:
-interface Todo {
+//Example:
+interface mTodo {
   title: string
   description: string
   completed: boolean
 }
 
-type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+type mTodoPreview = MyOmit<Todo, 'description' | 'title'>
 
-const todo: TodoPreview = {
+const mtodo: mTodoPreview = {
   completed: false,
-}*/
+}
+
+//My Attempt(Pass)
+type MyOmit<T, K> = { [P in keyof T as Exclude<P, K>]: T[P] }
+
+
+/*Solution 1: 
+type MyOmit<T, K> = { [P in keyof T as P extends K ? never : P]: T[P] };
+
+Solution 2:
+type MyOmit<T, K extends keyof T> = { [Key in Exclude<keyof T, K>]: T[Key] }*/
+
+
+/*Review: 
+I tried multiple solutions without using Exclude but didn't know how to disqualify keys or how to create a 
+conditional tenary inside an array. However, I knew using never to exclude keys would be the native solution*/
